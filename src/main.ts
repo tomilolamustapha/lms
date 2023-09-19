8dbe194f9aa66d3a83aadbafa8a7a5cf74c95045
LoluDayo <oluwatoni87@gmail.com>
2023-09-19 15:01:35 +0100

    started form styling

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
-  await app.listen(3000);
+  await app.listen(4004);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
