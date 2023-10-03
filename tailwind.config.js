a187c248e7f036cb7ad0ae0415681f286a045e62
LoluDayo <oluwatoni87@gmail.com>
2023-10-03 14:02:12 +0100

    New color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./views/**/*.{pug, html}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      'primary': '#6237A0',
+      'light': '#DEACF5',
+      'medium': '#9754CB',
+      'dark': '#28104E',
+    },
     extend: {},
   },
   plugins: [],
