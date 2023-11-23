d88430094ba8e6d2b994abef8eaa91e598297c1a
LoluDayo <oluwatoni87@gmail.com>
2023-11-23 12:24:14 +0100

    Admin tools

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+  constructor() {}
+
+  @Get('')
+  course(@Req() req: Request, @Res() res: Response) {
+    const message = res.locals.message;
+    res.render('course', { message });
+  }
+}
