046eba819fcedf8ed41f4b3f2c86f74685310252
LoluDayo <oluwatoni87@gmail.com>
2023-11-24 00:23:17 +0100

    dashboard stat fix

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -7,106 +7,97 @@ import { createUserDto } from 'src/user/dto/createUserDto';
 
 @Injectable()
 export class DashboardService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async AdminStats(id: number){
-        
-        const user = await this.prisma.user.findUnique({
-            where: { id },
-        });
+  async AdminStats(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
 
-        if (!user || user.role !== UserRole.Admin) {
-            throw new UnauthorizedException(
-                'Only admins can access these statistics.',
-            );
-        }
-
-        const course = await this.prisma.course.findUnique({
-            where: { id },
-        });
-
-        if (!course) {
-            throw new UnauthorizedException(
-                'Course not available',
-            );
-        }
-
-        //calculate total number of students
-        const numStudents = await this.prisma.user.count({
-            where: {
-                role: UserRole.Student,
-            },
-        });
-
-        console.log(numStudents)
-
-        //calculate total number of tutors
-        const numTutors = await this.prisma.user.count({
-            where: {
-                role: UserRole.Tutor,
-            },
-        });
-
-        console.log(numTutors)
-
-        // calculate total number of courses
-        const numCourses = this.prisma.course.count();
-
-        console.log(numCourses)
-        
-        return {
-            numCourses: numCourses ,
-            numStudents : numStudents,
-            numTutors : numTutors,
-            message: 'Total Users and Courses Fetched',
-        };
-
-        
+    if (!user || user.role !== UserRole.Admin) {
+      throw new UnauthorizedException(
+        'Only admins can access these statistics.',
+      );
     }
 
-    async recentUsers(data: dashboardDto) {
-        const { start_date, end_date } = data;
+    // const course = await this.prisma.course.findUnique({
+    //     where: { id },
+    // });
 
-        const paginate: PaginateFunction = paginator({});
+    // if (!course) {
+    //     throw new UnauthorizedException(
+    //         'Course not available',
+    //     );
+    // }
 
-        const startDate = new Date(start_date).toISOString();
+    //calculate total number of students
+    const numStudents = await this.prisma.user.count({
+      where: {
+        role: UserRole.Student,
+      },
+    });
 
-        const endDate = new Date(end_date).toISOString();
+    //calculate total number of tutors
+    const numTutors = await this.prisma.user.count({
+      where: {
+        role: UserRole.Tutor,
+      },
+    });
 
-        const recentlyRegisteredStudents = await this.prisma.user.findMany({
-            where: {
-                role: UserRole.Student,
-                createdAt: {
-                    gte: startDate,
-                    lte: endDate,
-                },
-            },
-            orderBy: {
-                createdAt: 'desc',
-            },
-            take: 10,
-        });
+    // calculate total number of courses
+    const numCourses = await this.prisma.course.count();
 
-        const recentlyRegisteredTutors = await this.prisma.user.findMany({
-            where: {
-                role: UserRole.Tutor,
-                createdAt: {
-                    gte: startDate,
-                    lte: endDate,
-                },
-            },
-            orderBy: {
-                createdAt: 'desc',
-            },
-            take: 10,
-        });
+    return {
+      message: 'Total Users and Courses Fetched',
+      numCourses,
+      numStudents,
+      numTutors,
+    };
+  }
 
-        return {
-            data: {
-                recentlyRegisteredStudents,
-                recentlyRegisteredTutors,
-            },
-            message: 'Recently Added Users Fetched',
-        };
-    }
+  async recentUsers(data: dashboardDto) {
+    const { start_date, end_date } = data;
+
+    const paginate: PaginateFunction = paginator({});
+
+    const startDate = new Date(start_date).toISOString();
+
+    const endDate = new Date(end_date).toISOString();
+
+    const recentlyRegisteredStudents = await this.prisma.user.findMany({
+      where: {
+        role: UserRole.Student,
+        createdAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 10,
+    });
+
+    const recentlyRegisteredTutors = await this.prisma.user.findMany({
+      where: {
+        role: UserRole.Tutor,
+        createdAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 10,
+    });
+
+    return {
+      data: {
+        recentlyRegisteredStudents,
+        recentlyRegisteredTutors,
+      },
+      message: 'Recently Added Users Fetched',
+    };
+  }
 }
