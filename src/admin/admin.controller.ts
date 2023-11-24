c5a47f24cdb903dcf1b8ddf7514a45b172c7af7a
LoluDayo <oluwatoni87@gmail.com>
2023-11-24 15:45:08 +0100

    admin integrations

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,20 +1,31 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UserGuard } from 'src/common/guards';
 import { DashboardService } from 'src/dashboard/dashboard.service';
+import { UserService } from 'src/user/user.service';
+import { AdminService } from './admin.service';
 @UseGuards(UserGuard)
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly dashboardService: DashboardService) {}
+  constructor(
+    private readonly adminService: AdminService,
+    private readonly dashboardService: DashboardService,
+    private readonly userService: UserService,
+  ) {}
+
   @Get('')
   async dashboard(@Req() req: Request, @Res() res: Response) {
     const message = res.locals.message;
     const payload: any = req.user;
     const stats = await this.dashboardService.AdminStats(payload.user.id);
+    const students = await this.adminService.getUserByRole('Student');
+    const tutors = await this.adminService.getUserByRole('Tutor');
     res.render('dashboard/admin', {
       message,
       user: payload.user,
       stats: stats,
+      students: students.users,
+      tutors: tutors.users,
     });
   }
 
@@ -30,18 +41,21 @@ export class AdminController {
   }
 
   @Get('settings/users')
-  users(@Req() req: Request, @Res() res: Response) {
+  async users(@Req() req: Request, @Res() res: Response) {
     const message = res.locals.message;
     const payload: any = req.user;
+    const users = await this.userService.getAllUsers();
+    console.log(users);
 
     res.render('users', {
       message,
       user: payload.user,
+      users: users.users,
     });
   }
 
   @Get('settings/users/add-user')
-  addUser(@Req() req: Request, @Res() res: Response) {
+  addUserPage(@Req() req: Request, @Res() res: Response) {
     const message = res.locals.message;
     const payload: any = req.user;
 
@@ -49,5 +63,25 @@ export class AdminController {
       message,
       user: payload.user,
     });
+  }
+
+  @Post('settings/users/add-user')
+  async addUser(@Req() req: Request, @Res() res: Response) {
+    const message = res.locals.message;
+    const payload: any = req.user;
+
+    try {
+      const user = await this.adminService.addUser(
+        req.body,
+        req.body.password,
+        req.body.username,
+      );
+
+      req.flash('success', user.message);
+      res.redirect('/admin/settings/users');
+    } catch (error) {
+      req.flash('error', error.message);
+      res.redirect('');
+    }
   }
 }
