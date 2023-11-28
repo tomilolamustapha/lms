b95ae87c03a26265acaf55d8c4b10309e7525d03
Tomilola <tomilolamustapha@gmail.com>
2023-11-28 14:54:53 +0100

    renewed hope

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -93,12 +93,12 @@ export class AdminController {
   }
 
   @Post('settings/users/:id/update-status')
-  async updatestatus(@Param('id') id: number, params :any , @Res() res: Response) {
+  async updatestatus(@Param('id') id: number, @Res() res: Response) {
     try {
       const userId = +id;
-      const user = await this.adminService.updateUserStatus(userId ,params);
-      console.log(user);
-      res.status(200).json({ message: user.message });
+      //const user = await this.adminService.updateUserStatus(userId);
+     // console.log(user);
+      //res.status(200).json({ message: user.message });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
